bcf205ddcbd2c5935eb686f3840866e623eab0a5
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-06-08 23:37:04 +0700

    'Comment input'

--- src/main/java/com/educare/open/controller/PostController.java
+++ src/main/java/com/educare/open/controller/PostController.java
@@ -1,7 +1,10 @@
 package com.educare.open.controller;
 
+import com.educare.open.model.Comment;
 import com.educare.open.model.Post;
+import com.educare.open.model.User;
 import com.educare.open.service.CategoryService;
+import com.educare.open.service.CommentService;
 import com.educare.open.service.PostRateService;
 import com.educare.open.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/post")
@@ -28,6 +33,8 @@ public class PostController {
     @Autowired
     private PostRateService postRateService;
 
+    @Autowired
+    CommentService commentService;
     @GetMapping
     public ModelAndView findAllByOrderByIdDesc(@PageableDefault(value = 10) Pageable pageable) {
         ModelAndView modelAndView = new ModelAndView("index");
@@ -69,12 +76,23 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public ModelAndView view(@PathVariable("id") Integer id) {
+    public ModelAndView view(@PathVariable("id") Integer id,HttpSession session) {
         ModelAndView modelAndView = new ModelAndView("posts/viewPost");
+        List<Comment> comments = commentService.findAllByPostID(id);
+        modelAndView.addObject("comments",comments);
         modelAndView.addObject("post", postService.findById(id));
-        modelAndView.addObject("isRead", postService.isRead(null, id));
+        modelAndView.addObject("isRead", postService.isRead(((User)session.getAttribute("currentUser")).getId(), id));
         modelAndView.addObject("rating", postRateService.avgRateByPostId(id));
+        modelAndView.addObject("comments",comments);
         return modelAndView;
     }
+    @PostMapping("/{id}/checkRead")
+    public String checkRead(HttpSession session,@PathVariable("id") Integer id, RedirectAttributes redirectAttributes){
 
+        User currentUser = (User) session.getAttribute("currentUser");
+        redirectAttributes.addFlashAttribute("isRead", postService.isRead(
+          currentUser.getId(), id));
+//        postRateService.save(currentUser, id);
+        return "redirect:/post/"+id;
+    }
 }
--- src/main/java/com/educare/open/service/impl/UserPostServiceImpl.java
+++ src/main/java/com/educare/open/service/impl/UserPostServiceImpl.java
@@ -1,6 +1,21 @@
 package com.educare.open.service.impl;
 
+import com.educare.open.model.User;
+import com.educare.open.model.UserPost;
+import com.educare.open.repository.PostRepository;
+import com.educare.open.repository.UserPostRepository;
 import com.educare.open.service.UserPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserPostServiceImpl implements UserPostService {
+  @Autowired
+  private UserPostRepository userPostRepository;
+
+//  @Autowired
+//  private PostRepository userPostRepository;
+//  @Override
+//  public void save(User user, Integer postId) {
+//    UserPost userPost = new UserPost(true, user)
+//    userPostRepository.save(userPost);
+//  }
 }
