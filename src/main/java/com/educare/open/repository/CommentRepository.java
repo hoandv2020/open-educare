61e146eeed40ad695823a65de2f1c3fe9f36a17d
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-06-08 15:42:18 +0700

    'Find all categories'

--- src/main/java/com/educare/open/repository/CommentRepository.java
+++ src/main/java/com/educare/open/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.educare.open.repository;
 import com.educare.open.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+  List<Comment> getCommentsByPostId(Integer postId);
 }
