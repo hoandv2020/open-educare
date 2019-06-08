e56f72b2657d81b4053b6a48fe59f7873adf14f6
hoan <hoandv2020@gmail.com>
2019-06-08 10:44:36 +0700

    Add all controllers

---
+++ src/main/java/com/educare/open/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.educare.open.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+}
