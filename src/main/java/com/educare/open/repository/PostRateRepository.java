1c6a281b821fcee72a5549b9b6ebd93b8689b8c5
minh <minhdotcom@gmail.com>
2019-06-08 16:13:52 +0700

    getRateByPostId (unfinished)

--- src/main/java/com/educare/open/repository/PostRateRepository.java
+++ src/main/java/com/educare/open/repository/PostRateRepository.java
@@ -1,7 +1,10 @@
 package com.educare.open.repository;
 
 import com.educare.open.model.Post;
+import com.educare.open.utils.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PostRateRepository extends JpaRepository<Post, Integer> {
 }
