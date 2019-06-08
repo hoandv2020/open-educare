e4b115d8ab676ca0d1058f34a9f6c4575129fbf6
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-06-08 17:55:43 +0700

    'View Post'

--- src/main/java/com/educare/open/service/impl/PostRateServiceImpl.java
+++ src/main/java/com/educare/open/service/impl/PostRateServiceImpl.java
@@ -8,20 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 public class PostRateServiceImpl implements PostRateService {
-    @Autowired
-    PostRateRepository postRateRepository;
+  @Autowired
+  PostRateRepository postRateRepository;
 
-    @Override
-    public float avgRateByPostId(Integer postID) {
-        List<PostRate> postRates = postRateRepository.findAllByPostId(postID);
-        if (postRates == null) {
-            return 0;
-        }
-        float sum = 0;
-        for (PostRate postRate : postRates) {
-            sum += postRate.getRate();
-        }
-
-        return sum / postRates.size();
+  @Override
+  public float avgRateByPostId(Integer postID) {
+    List<PostRate> postRates = postRateRepository.findAllByPostId(postID);
+    if (postRates.size() == 0) {
+      return 0;
     }
+    float sum = 0;
+    for (PostRate postRate : postRates) {
+      sum += postRate.getRate();
+    }
+
+    return sum / postRates.size();
+  }
 }
