53193eea971bf7356473fd4c987e95bcff8209a8
thinhluffy <thinhle24081995@gmail.com>
2019-06-09 01:04:01 +0700

    Update demo.

--- src/main/java/com/educare/open/controller/PostController.java
+++ src/main/java/com/educare/open/controller/PostController.java
@@ -5,16 +5,15 @@ import com.educare.open.service.CategoryService;
 import com.educare.open.service.PostRateService;
 import com.educare.open.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/post")
