ebbafaee983bf58a3944d2b297c95ac82682cb66
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-06-09 10:41:15 +0700

    check login

--- src/main/java/com/educare/open/controller/CommentController.java
+++ src/main/java/com/educare/open/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.educare.open.controller;
 
 import com.educare.open.model.Comment;
+import com.educare.open.model.User;
 import com.educare.open.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -18,10 +20,14 @@ public class CommentController {
   @Autowired
   CommentService commentService;
   @GetMapping("/{postId}")
-  public ModelAndView listComments(@PathVariable Integer postId){
+  public ModelAndView listComments(@PathVariable Integer postId, HttpSession httpSession){
     ModelAndView modelAndView = new ModelAndView("testListComment");
     List<Comment> comments = commentService.findAllByPostID(postId);
     modelAndView.addObject("comments",comments);
+    User currentUser = (User)httpSession.getAttribute("currentUser");
+    boolean isLogin = false;
+    if (currentUser!=null) isLogin =true;
+    modelAndView.addObject("isLogin",isLogin);
     return modelAndView;
   }
 }
